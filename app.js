07ff25ecca3a6070b4ccad19ab5f2f89811a4d6a
Paula Deffas <paulaalejandradeffas@gmai.com>
2026-09-04 14:11:07 -0500

    leer archivo json

--- app.js
+++ app.js
@@ -9,14 +9,23 @@ res.send('API REST APRENDICES');
 
 //midelware para parsear datos del body
 app.use(express.json()); 
-
 app.use(express.urlencoded({ extended: true }));
+
+//leer archivo
+const sistemaArchivo = require('fs');
+const ruta = require('path');
+const rutaArchivo = ruta.join(__dirname, 'datos.json');
+
 
 //endpoint para  listar de aprendices
 
 app.get("/api/aprendices", (req, res) => {
-  res.status(200).json({
-    "mensaje": "Lista de aprendices:)"
+    sistemaArchivo.readFile(rutaArchivo, 'utf-8', (error, datos) => {
+        if (error) {
+            return res.status(500).json({ mensaje: "Error al leer el archivo" });
+        }
+        const listaAprendices = JSON.parse(datos)     
+        res.status(200).json({"mensaje": listaAprendices})
   })
 })
 
@@ -31,10 +40,24 @@ app.get("/api/aprendices/:id", (req, res) => {
 //endpoint para crear un aprendiz
 
 app.post("/api/aprendices", (req, res) => {
-  res.status(201).json({
-    "mensaje": "Crear aprendices"
-  })
-})
+    const nuevoAprendiz = req.body;
+
+    //leer archivo y agregar un nuevo aprendiz
+    sistemaArchivo.readFile(rutaArchivo, 'utf-8', (error, datos) => {
+        if (error) {
+            return res.status(500).json({ mensaje: "Error al leer el archivo" });
+        }
+        const listaAprendices = JSON.parse(datos)     
+        // agregar el nuevo aprendiz al arreglo
+        listaAprendices.push(nuevoAprendiz);
+        sistemaArchivo.writeFile(rutaArchivo, JSON.stringify(listaAprendices, null, 2), (error) => {
+            if (error) {
+                return res.status(500).json({ mensaje: "no se puede escribir en el archivo, o BD" });
+            }
+        res.status(200).json({"mensaje": "Aprendiz creado", "datos aprendiz": nuevoAprendiz})
+    });
+  });
+});
 
 //endpoint para actualizar un aprendices
 
